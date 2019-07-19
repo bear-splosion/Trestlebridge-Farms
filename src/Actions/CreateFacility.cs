0966f2dfe04c2ba10d3e10d678f3f1821a36c783
Heather Cleland <heathercleland@gmail.com>
2019-07-19 14:00:29 -0500

    first ticket resolved

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -3,17 +3,23 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class CreateFacility {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Grazing field");
-            Console.WriteLine ("2. Plowed field");
+namespace Trestlebridge.Actions
+{
+    public class CreateFacility
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Grazing field");
+            Console.WriteLine("2. Plowed field");
+            Console.WriteLine("3. Natural field");
+            Console.WriteLine("4. Chicken House");
+            Console.WriteLine("5. Duck House");
 
-            Console.WriteLine ();
-            Console.WriteLine ("Choose what you want to create");
+            Console.WriteLine();
+            Console.WriteLine("Choose what you want to create");
 
-            Console.Write ("> ");
-            string input = Console.ReadLine ();
+            Console.Write("> ");
+            string input = Console.ReadLine();
 
             switch (Int32.Parse(input))
             {
@@ -23,6 +29,7 @@ namespace Trestlebridge.Actions {
                 default:
                     break;
             }
+
         }
     }
 }
